11abd2acae1227522b91017fbd393ee110bbe320
anishDalvi <anishdalvi2001@gmail.com>
2023-10-15 15:58:55 +0530

    md to pdf function working if path is specified in backend

--- backend/convert.js
+++ backend/convert.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const { mdToPdf } = require('md-to-pdf');
 
 (async () => {
-	//const pdf = await mdToPdf({ path: 'markdown.md' }).catch(console.error);
-	const pdf = await mdToPdf({ content: '# Hello, World' }, { dest: 'output.pdf' }).catch(console.error);
+	const pdf = await mdToPdf(
+    { path: "./backend/markdown.md" },
+    { dest: "output.pdf" }
+  ).catch(console.error);
+	//const pdf = await mdToPdf({ content: '# Hello, World 3' }, { dest: 'output.pdf' }).catch(console.error);
 
 	if (pdf) {
 		fs.writeFileSync(pdf.filename, pdf.content);
